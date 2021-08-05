44dc2e824e955682fd4c4de51afdef515fb4f3b5
janchua19 <jan.edward.chua@oocl.com>
2021-08-05 14:16:15 +0800

    [FizzBuzzWhizz][Jan] feat: implement method for muplitple of 3 (should_return_Fizz_when_countOff_given_number_multiple_of_3)

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(Integer number) {
-
+//        if(number % 3 == 0)
+//        {
+//            return "Fizz";
+//        }
         return Integer.toString(number);
     }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -19,4 +19,13 @@ public class FizzBuzzTest {
         assertThat(result).isEqualTo("1");
     }
 
+    @Test
+    void should_return_Fizz_when_countOff_given_number_multiple_of_3(){
+        int number = 13;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.countOff(number);
+
+        assertThat(result).isEqualTo("Fizz");
+    }
 }
