d441cbdddb497317e3b0656239ee66771efecd8a
janchua19 <jan.edward.chua@oocl.com>
2021-08-05 13:54:54 +0800

    [FizzBuzzWhizz][Jan] feat: implement normal case (should_number_when_countOff_given_1)

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_number_when_countOff_given_1() {
+        //should
+        int number = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String result = fizzBuzz.countOff();
+
+        //then
+        assertThat(result).equals("1");
+    }
 }
